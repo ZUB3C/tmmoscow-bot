82454a3497a774972cc498bff16731799d0cfc14
ZUB3C <pitzubarev@yandex.ru>
2024-06-11 23:32:11 +0300

    Removed useless files

--- tmmoscow_api/models/base/model.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel as PydanticBaseModel
-from pydantic.alias_generators import to_camel
-
-
-class BaseModel(PydanticBaseModel):
-    class Config:
-        alias_generator = to_camel
-        populate_by_name = True
