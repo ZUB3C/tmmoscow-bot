2a6c77c6d98c8c471562762a34adddd7a91d0cdd
ZUB3C <pitzubarev@yandex.ru>
2024-06-22 21:47:03 +0300

    Fix _get function by specifying user-agent and encoding

--- tmmoscow_api/consts.py
+++ tmmoscow_api/consts.py
@@ -1,6 +1,19 @@
 from zoneinfo import ZoneInfo
 
+from .enums import DistanceType
+
 TIMEZONE = ZoneInfo("Europe/Moscow")
 BASE_URL = "http://www.tmmoscow.ru"
 INDEX_PATH = "/index.php"
 INDEX_URL = f"{BASE_URL}{INDEX_PATH}"
+
+DEFAULT_HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+    )
+}
+
+HTML_ENCODING = "cp1251"
+
+ID_TO_DISTANCE_TYPE = {distance_type.id: distance_type for distance_type in DistanceType}
